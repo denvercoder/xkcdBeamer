a9ae987c6fa7c42c85a5dd9f882bf8f02f535cfa
Tim Myers <timmyers1978@gmail.com>
2016-10-14 14:20:31 -0600

    Initial Commit

---
+++ xkcdBeamer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xkcdBeamer
+//
+//  Created by Timothy Myers on 10/14/16.
+//  Copyright © 2016 okayestprogrammer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
